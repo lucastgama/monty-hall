e47c4bca41eab4702317e64e0bb87e2d7d6ca020
lucastgama <lucas_tgama@hotmail.com>
2023-05-25 13:34:46 -0300

    componentes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Problema Monty Hall</h1>
+    <div class="form">
+      <div v-if="!started">
+        <label for="portsAmount">Quantas portas ?</label>
+        <input
+          type="text"
+          size="3"
+          id="portsAmount"
+          v-model.number="portsAmount"
+        />
+      </div>
+      <div v-if="!started">
+        <label for="selectedPort"> Qual é a porta premiada ?</label>
+        <input
+          type="text"
+          id="selectedPort"
+          size="3"
+          v-model.number="selectedPort"
+        />
+      </div>
+      <button v-if="!started" @click="started = true">Iniciar</button>
+      <button v-if="started" @click="started = false">Reiniciar</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in portsAmount" :key="i">
+        <DoorSelected :hasGift="i === selectedPort" :number="i" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DoorSelected from "./components/DoorOpen.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    DoorSelected,
+  },
+  data: function () {
+    return {
+      started: false,
+      portsAmount: 3,
+      selectedPort: null,
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+body {
+  color: #fff;
+  background: linear-gradient(to right, rgba(21, 153, 87), rgba(21, 87, 153));
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#app h1 {
+  border: 1px solid #111;
+  background-color: #1114;
+  padding: 20px;
+  margin-bottom: 60px;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-self: center;
+  align-items: center;
+  margin-bottom: 40px;
+}
+.form,
+.form input,
+.form button {
+  margin-bottom: 10px;
+  font-size: 2rem;
+}
+.doors {
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/DoorOpen.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="door-area">
+    <div class="door-frame" :class="{ selected: selected && !open }">
+      <GiftComponent v-if="open && hasGift" />
+    </div>
+    <div class="door" :class="{ open }" @click="selected = !selected">
+      <div class="number" :class="{ selected }">{{ number }}</div>
+      <div class="knoob" :class="{ selected }" @click="open = true"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GiftComponent from "./GiftComponent.vue";
+
+export default {
+  name: "DoorSelected",
+  components: { GiftComponent },
+  props: {
+    number: {},
+    hasGift: { type: Boolean },
+  },
+  data: function () {
+    return {
+      open: false,
+      selected: false,
+    };
+  },
+};
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+.door-area {
+  position: relative;
+  width: 200px;
+  height: 310px;
+  border-bottom: 10px solid #aaa;
+  margin-bottom: 20px;
+  font-size: 3rem;
+
+  display: flex;
+  justify-content: center;
+}
+
+.door-frame {
+  position: absolute;
+  height: 300px;
+  width: 180px;
+  border-left: 3px solid brown;
+  border-right: 3px solid brown;
+  border-top: 3px solid brown;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+}
+.door {
+  position: absolute;
+  top: 5px;
+  height: 295px;
+  width: 170px;
+  background-color: chocolate;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+.door .knoob {
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
+  background-color: brown;
+  align-self: flex-start;
+  margin-top: 60px;
+}
+.door-frame.selected {
+  border-left: 3px solid goldenrod;
+  border-right: 3px solid goldenrod;
+  border-top: 3px solid goldenrod;
+}
+.door .number.selected {
+  color: goldenrod;
+}
+.door .knoob.selected {
+  background-color: goldenrod;
+}
+.door.open {
+  background-color: #0007;
+}
+.door.open .knoob {
+  display: none;
+}
+.door.open .number {
+  display: none;
+}
+</style>
